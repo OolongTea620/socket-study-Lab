8fb24e90ca9c6c5ec4145287b1405bde3af0dde9
OolongTea620 <rlafls9596@gmail.com>
2023-10-05 20:44:04 +0900

    🎉: init project Setting

---
+++ src/main/java/com/bid/soketstudy/SoketStudyApplication.java
@@ -0,0 +1,13 @@
+package com.bid.soketstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoketStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoketStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bid/soketstudy/SoketStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bid.soketstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoketStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
